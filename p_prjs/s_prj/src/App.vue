9385c8c41507e48a5f4301715a29258548257d36
eternitylsh <eternialsh32@naver.com>
2023-11-17 17:44:00 +0900

    뷰 만든거 일단 커밋;;

---
+++ p_prjs/s_prj/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <button>숫자 세기: {{ count }}</button>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import nice_p01 from './components/nice.vue'
+
+export default {
+  name: 'App',
+  components: {
+    nice_p01,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ p_prjs/s_prj/src/components/nice_p01.vue
@@ -0,0 +1,9 @@
+<template>
+    <button>숫자 세기: {{ count }}</button>
+  </template>
+
+<script>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
