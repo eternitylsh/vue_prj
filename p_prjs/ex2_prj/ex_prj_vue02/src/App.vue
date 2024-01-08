cc4777e6bcd319c0271d8e280a349271d51a139e
eternitylsh <eternialsh32@naver.com>
2024-01-08 17:50:09 +0900

    ...

---
+++ p_prjs/ex2_prj/ex_prj_vue02/src/App.vue
@@ -0,0 +1,85 @@
+<script setup>
+  import { reactive } from 'vue';
+
+  const sign = reactive({
+    title: "header",
+    nav: "nav",
+    section: "section",
+    aside: "aside",
+    footer: "footer"
+  })
+</script>
+
+<template>
+  <header>
+    {{ sign.title }}
+  </header>
+  <nav>
+    {{ sign.nav }}
+  </nav>
+  <main>
+    <section>{{ sign.section }}</section>
+    <aside>{{ sign.aside }}</aside>
+  </main>
+  <footer>{{ sign.footer }}</footer>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 12%;
+
+  background-color: #365486;
+  color : white;
+}
+
+nav {
+  width: 100%;
+  height: 8%;
+
+  background-color:#DCF2F1;
+}
+
+main {
+  width: 100%;
+  height: 68%;
+
+  display: flex;
+  flex-direction: row;
+}
+
+main section {
+  width: 75%;
+  background-color:#7FC7D9;
+}
+
+main aside {
+  width: 25%;
+  background-color: #365486;
+
+  color : white;
+}
+
+footer {
+  width: 100%;
+  height: 12%;
+
+  background-color: #0F1035;
+
+  color : white;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    /* place-items: center; */
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
