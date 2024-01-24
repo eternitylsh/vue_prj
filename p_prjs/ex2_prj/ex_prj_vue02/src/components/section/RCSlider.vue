d856c31b4a01919cd7528d348cb7bfa879ca3101
eternitylsh <eternialsh32@naver.com>
2024-01-24 17:48:13 +0900

    일단 커밋.. vm은 아직 미완성;;

--- p_prjs/ex2_prj/ex_prj_vue02/src/components/section/RCSlider.vue
+++ p_prjs/ex2_prj/ex_prj_vue02/src/components/section/RCSlider.vue
@@ -3,35 +3,35 @@
     import Slider from '@vueform/slider'
 
     const value = reactive([25, 75])
-    console.log(value.target)
+
+    const emit = defineEmits(['svalues'])
+    emit('svalues', value)
 </script>
 
 <template>
-    <div id="ntime-view">
-        <span>00:00:00:000</span>
-    </div>
     <div id="multi-slider">
-     <Slider v-model="value"
+     <Slider v-model="value" class="slider-blue"
         :min="0"
         :max="100"
         :step="0.1"
-        
+        :tooltips="false"
+        :classes="{
+            horizontal: 'slider-horizontal h-2.5',
+        }"
      />
     </div>
 </template>
 
 <style src="@vueform/slider/themes/default.css"></style>
 <style>
-#ntime-view {
-    margin: 0 auto;
-    text-align: center;
-}
-#ntime-view span {
-    font-size: 1.2rem;
+#multi-slider {
+    margin: 1.2rem auto;
+    width: 50%;
 }
 
-#multi-slider {
-    margin-top: 2.6rem;
-    width: 50%;
+.slider-blue {
+  --slider-connect-bg: #3B82F6;
+  --slider-tooltip-bg: #3B82F6;
+  --slider-handle-ring-color: #3B82F630;
 }
 </style>
--- p_prjs/ex2_prj/ex_prj_vue02/src/components/section/ViewMovie.vue
+++ p_prjs/ex2_prj/ex_prj_vue02/src/components/section/ViewMovie.vue
@@ -1,18 +1,54 @@
 <script setup>
-    import { ref } from 'vue'
+    import { ref, reactive } from 'vue'
     import vcon from './RCSlider.vue'
 
     const isClicked = ref('false')
 
+    const videoObj = ref({
+        src: "../../assets/movie/Ice_Bear.mp4",
+        preload: "metadata",
+        muted: true,
+        playsinline: true,
+
+        currentTime: 0
+    })
+
+    const ntimeview = reactive({
+        videoctime: 0,
+        min: {
+            h: 0,
+            m: 0,
+            s: 0,
+            ss: 0,
+            str: `${this.h}:${this.m}:${this.s}:${this.ss}`
+        },
+        max: {
+            h: 0,
+            m: 0,
+            s: 0,
+            ss: 0,
+            str: `${this.h}:${this.m}:${this.s}:${this.ss}`
+        },
+
+        OnUpdate(__v) {
+            // __v[0] : min
+            // __v[1] : max
+        },
+    })
+
     const movieupdate = __v => {
     
         // 추후.. 원천 막을방법에 관해 고민을..
-        if( max_obj.value < min_obj.value ){ max_obj.value = min_obj.value; }
+        // if( max_obj.value < min_obj.value ){ max_obj.value = min_obj.value; }
         // console.log( `${max_obj.value}, ${min_obj.value}`  )
         
-        if( isCLicked ) {
-            video_obj.currentTime = ( video_obj.duration / 100 ) * __v;
-            movie_HTMLtimeline_update( video_obj.currentTime )
+        
+
+        video_obj.currentTime = ( video_obj.duration / 100 ) * __v;
+        movie_HTMLtimeline_update( video_obj.currentTime )
+
+        if( isClicked ) {
+            
         }
     }
 
@@ -24,22 +60,18 @@
         // tofixed(3) 의 숫자 3은 UI상 표시되는 second이하 수치.
         ntime_view.innerHTML = `${_h}:${_m}:${_s}:${_ss}`;
     }
-
-    const OnMdown = () => {
-        isClicked.value = true;
-    }
-
-    const onMup = () => {
-        isCLicked.value = false
-    }
 </script>
 
 <template>
     <div id="video_center">
         <div id="videoview" class="flex_center">
-            <video class="video" src="../../assets/movie/Ice_Bear.mp4" frameborder="0" preload="metadata" muted playsinline />
+            <video class="video" ref="videoObj" @timeupdate="currentTime_ = $event.target.currentTime" />
         </div>
-        <vcon />
+        <div id="ntime-view">
+            <span>{{ ntimeview.min.str }}</span>
+            <span>{{ ntimeview.max.str }}</span>
+        </div>
+        <vcon @svalues="__vs => movieupdate(__vs)" />
         <testdrag />
     </div>
 </template>
@@ -62,5 +94,14 @@
 
 #videopannel-control {
     margin-top: 30px;
+}
+
+#ntime-view {
+    margin: 0 auto;
+    text-align: center;
+}
+#ntime-view span {
+    font-size: 1.2rem;
+    margin: 0.5rem;
 }
 </style>
